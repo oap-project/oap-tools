08ca0a722965929cde3ffa3ad0fb4d496c7f88d1
Qing Yao <Qing.Yao@intel.com>
2021-06-03 17:28:40 +0800

    Add build profile for mllib

--- dev/compile-oap.sh
+++ dev/compile-oap.sh
@@ -108,7 +108,7 @@ function build_oap(){
     cd $OAP_HOME/oap-mllib/mllib-dal
     source /opt/intel/oneapi/setvars.sh
     source /tmp/oneCCL/build/_install/env/setvars.sh
-    mvn clean package  -DskipTests
+    mvn clean package  -DskipTests  -Pspark-3.1.1
     ;;
 
     pmem-common)    
