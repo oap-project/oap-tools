910d5a58e9a0f0628349d4ab236d27e6f7d59dc1
zhixingheyi-tian  <xiangxiang.shen@intel.com>
2021-07-28 19:38:25 +0800

    [OAP-TOOL-100]Reorganize Kubernetes directory and update EKS integration

---
+++ integrations/oap/kubernetes/integration-tests/oap/Kmeans-example.scala
@@ -0,0 +1,26 @@
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.evaluation.ClusteringEvaluator
+
+spark.sparkContext.setLogLevel("INFO")
+
+val dataset = spark.read.format("libsvm").load("/opt/home/spark-3.1.1/data/mllib/sample_kmeans_data.txt")
+
+// Trains a k-means model.
+val kmeans = new KMeans().setK(2).setSeed(1L)
+val model = kmeans.fit(dataset)
+
+// Make predictions
+val predictions = model.transform(dataset)
+
+// Evaluate clustering by computing Silhouette score
+val evaluator = new ClusteringEvaluator()
+
+val silhouette = evaluator.evaluate(predictions)
+println(s"Silhouette with squared euclidean distance = $silhouette")
+
+// Shows the result.
+println("Cluster Centers: ")
+model.clusterCenters.foreach(println)
+ 
+
+
