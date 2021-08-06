eb5349d367a5a72fd157897644480ec99efd08a2
HongW2019 <hong2.wang@intel.com>
2021-08-06 14:49:51 +0800

    [OAP-TOOL-96]Update Dataproc notebooks and scripts

--- integrations/oap/dataproc/bootstrap_oap.sh
+++ integrations/oap/dataproc/bootstrap_oap.sh
@@ -3,17 +3,9 @@
 oap_install_dir=/opt/benchmark-tools/oap
 sudo mkdir -p $oap_install_dir
 conda_install_dir=/opt/benchmark-tools/conda
-sudo yum -y install wget
-
-## Step 1: install conda
-sudo wget -c https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -P /tmp
-sudo chmod +x /tmp/Miniconda2-latest-Linux-x86_64.sh
-sudo bash /tmp/Miniconda2-latest-Linux-x86_64.sh -b -p ${conda_install_dir}
-${conda_install_dir}/bin/conda init
-source ~/.bashrc
 sudo chown $(whoami):$(whoami) ${oap_install_dir}
 
-## Step 2: install oap
+## conda install oap
 
-conda create -p ${oap_install_dir}  -c conda-forge -c intel-beaver -c intel -c intel-bigdata -y oap=1.2.0
+conda create -p ${oap_install_dir}  -c conda-forge -c intel-beaver -c intel -c intel-bigdata -y oap=1.2.0.dataproc20
 
