d0a99d5aa183ec4c4f04ff823a8d15e7c3b83236
Qing Yao <qing.yao@intel.com>
2021-08-23 13:30:48 +0800

    [OAP-TOOLS-112]Update tensorflow version for ML 8.*

--- integrations/ml/databricks/init_intel_optimized_ml.sh
+++ integrations/ml/databricks/init_intel_optimized_ml.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [ -n $DATABRICKS_ROOT_CONDA_ENV ]; then
-  if [ $DATABRICKS_RUNTIME_VERSION == "7.6" ] ; then
-
+  if [ $DATABRICKS_RUNTIME_VERSION == "7.3" ] ; then
     echo 'export USE_DAAL4PY_SKLEARN=YES' >> ~/.bashrc
     conda install -c intel scikit-learn pandas 
     pip install intel-tensorflow==2.3.0
--- integrations/ml/databricks/init_intel_optimized_ml_ex.sh
+++ integrations/ml/databricks/init_intel_optimized_ml_ex.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+VERSION_8_P='^8.*$'
 
-pip install intel-tensorflow==2.3.0
-pip install scikit-learn-intelex
+if [[ $DATABRICKS_RUNTIME_VERSION == "7.3" ]]; then
+    pip install intel-tensorflow==2.3.0
+    pip install scikit-learn-intelex
+elif [[ $DATABRICKS_RUNTIME_VERSION =~ $VERSION_8_P ]]; then
+    pip install intel-tensorflow==2.4.0
+    pip install scikit-learn-intelex
+fi
