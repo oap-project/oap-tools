4c500bed808d9760769d262c24c860561dc5a934
Qing Yao <qing.yao@intel.com>
2021-08-29 21:15:13 +0800

    [OAP-TOOLS-112] Update Intel Optimized ML init scripts about to runtime version 9.*

--- integrations/ml/databricks/init_intel_optimized_ml_ex.sh
+++ integrations/ml/databricks/init_intel_optimized_ml_ex.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 VERSION_8_P='^8.*$'
-
+VERSION_9_P='^9.*$'
 if [[ $DATABRICKS_RUNTIME_VERSION == "7.3" ]]; then
     pip install intel-tensorflow==2.3.0
     pip install scikit-learn-intelex
 elif [[ $DATABRICKS_RUNTIME_VERSION =~ $VERSION_8_P ]]; then
     pip install intel-tensorflow==2.4.0
+    pip install scikit-learn-intelex
+elif [[ $DATABRICKS_RUNTIME_VERSION =~ $VERSION_9_P ]]; then
+    pip install intel-tensorflow==2.5.0
     pip install scikit-learn-intelex
 fi
