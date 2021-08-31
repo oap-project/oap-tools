f23b2c53bd6df0220dbebedae878eee670294f2c
Qing Yao <qing.yao@intel.com>
2021-08-31 11:36:27 +0800

    [OAP-TOOLS-112]Update tf version for Databricks Runtime ML 8.4

--- integrations/ml/databricks/init_intel_optimized_ml_ex.sh
+++ integrations/ml/databricks/init_intel_optimized_ml_ex.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION_8_P='^8.*$'
+VERSION_8_P='^8.[0-3]$'
 VERSION_9_P='^9.*$'
 if [[ $DATABRICKS_RUNTIME_VERSION == "7.3" ]]; then
     pip install intel-tensorflow==2.3.0
@@ -7,7 +7,7 @@ if [[ $DATABRICKS_RUNTIME_VERSION == "7.3" ]]; then
 elif [[ $DATABRICKS_RUNTIME_VERSION =~ $VERSION_8_P ]]; then
     pip install intel-tensorflow==2.4.0
     pip install scikit-learn-intelex
-elif [[ $DATABRICKS_RUNTIME_VERSION =~ $VERSION_9_P ]]; then
+elif [[ $DATABRICKS_RUNTIME_VERSION =~ $VERSION_9_P ]] || [[ $DATABRICKS_RUNTIME_VERSION == "8.4" ]]; then
     pip install intel-tensorflow==2.5.0
     pip install scikit-learn-intelex
 fi
