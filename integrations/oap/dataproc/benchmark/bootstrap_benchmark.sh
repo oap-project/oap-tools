f918d50c40c6edcedea0787977d5fa469c1a1c78
HongW2019 <hong2.wang@intel.com>
2021-09-26 15:43:48 +0800

    [OAP-TOOL-174]Update OAP init scripts on Dataproc

--- integrations/oap/dataproc/benchmark/bootstrap_benchmark.sh
+++ integrations/oap/dataproc/benchmark/bootstrap_benchmark.sh
@@ -36,10 +36,6 @@ function install_tpcds_kit() {
   make OS=LINUX
 }
 
-function install_tpch_dbgen() {
-  cd ${SOFTWARE_HOME} && git clone https://github.com/databricks/tpch-dbgen
-  cd tpch-dbgen && make clean && make;
-}
 
 function install_hibench() {
   install_maven
@@ -49,14 +45,7 @@ function install_hibench() {
   else
     cd HiBench && git pull
   fi
-  cp hadoopbench/mahout/pom.xml hadoopbench/mahout/pom.xml.bak
-  cat hadoopbench/mahout/pom.xml \
-      | sed 's|<repo2>http://archive.cloudera.com</repo2>|<repo2>https://archive.apache.org</repo2>|' \
-      | sed 's|cdh5/cdh/5/mahout-0.9-cdh5.1.0.tar.gz|dist/mahout/0.9/mahout-distribution-0.9.tar.gz|' \
-      | sed 's|aa953e0353ac104a22d314d15c88d78f|09b999fbee70c9853789ffbd8f28b8a3|' \
-      > ./pom.xml.tmp
-  mv ./pom.xml.tmp hadoopbench/mahout/pom.xml
-  mvn -Psparkbench -Dmodules -Pml -Pmicro -Dspark=3.0 -Dscala=2.12 -DskipTests clean package
+  mvn -Psparkbench -Dmodules -Pml -Pmicro -Dspark=3.1 -Dscala=2.12 -DskipTests clean package
 }
 
 function install_tpcds() {
@@ -64,13 +53,9 @@ function install_tpcds() {
   install_tpcds_kit
 }
 
-function install_tpch() {
-  install_spark_sql_perf
-  install_tpch_dbgen
-}
 
 function usage() {
-  echo "Usage: $0 --all|--tpcds|--tpch|--hibench|-h|--help" >&2
+  echo "Usage: $0 --all|--tpcds|--hibench|-h|--help" >&2
 }
 
 
@@ -83,17 +68,11 @@ case $key in
     echo "Start to deploy all benchmark for OAP ..."
     install_hibench
     install_tpcds
-    install_tpch
     exit 0
     ;;
     --tpcds)
     shift 1
     install_tpcds
-    exit 0
-    ;;
-    --tpch)
-    shift 1
-    install_tpch
     exit 0
     ;;
     --hibench)
