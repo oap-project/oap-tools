bc8e55ebbcd20b3e302e24ef99babeacaa54d666
Qing Yao <qing.yao@intel.com>
2021-10-11 09:57:31 +0800

    [OAP-TOOLS-112]Install scikit-learn-intelex for more runtime.

--- integrations/ml/databricks/init_intel_optimized_ml.sh
+++ integrations/ml/databricks/init_intel_optimized_ml.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-if [ -n $DATABRICKS_ROOT_CONDA_ENV ]; then
-  if [ $DATABRICKS_RUNTIME_VERSION == "7.3" ] ; then
+VERSION_8_P='^8.[0-3]$'
+VERSION_9_P='^9.*$'
+
+if [[ $DATABRICKS_RUNTIME_VERSION == "7.3" ]]; then
+  if [ -n $DATABRICKS_ROOT_CONDA_ENV ]; then
     echo 'export USE_DAAL4PY_SKLEARN=YES' >> ~/.bashrc
     conda install -c intel scikit-learn pandas 
     pip install intel-tensorflow==2.3.0 
+  else
+    pip install intel-tensorflow==2.3.0
+    pip install scikit-learn-intelex
   fi
+elif [[ $DATABRICKS_RUNTIME_VERSION =~ $VERSION_8_P ]]; then
+    pip install intel-tensorflow==2.4.0
+    pip install scikit-learn-intelex
+elif [[ $DATABRICKS_RUNTIME_VERSION =~ $VERSION_9_P ]] || [[ $DATABRICKS_RUNTIME_VERSION == "8.4" ]]; then
+    pip install intel-tensorflow==2.5.0
+    pip install scikit-learn-intelex
 fi
