c5380245d6198761ac0e51e5f56cfcb101f43fe2
Qing Yao <qing.yao@intel.com>
2022-06-21 08:56:54 +0800

    Update for databricks new runtime.

--- integrations/ml/databricks/init_intel_optimized_ml.sh
+++ integrations/ml/databricks/init_intel_optimized_ml.sh
@@ -12,13 +12,13 @@ if [[ $DATABRICKS_RUNTIME_VERSION == "7.3" ]]; then
     pip install intel-tensorflow==2.3.0
     pip install scikit-learn-intelex
   fi
-elif [[ $DATABRICKS_RUNTIME_VERSION =~ $VERSION_8_P ]]; then
-    pip install intel-tensorflow==2.4.0
-    pip install scikit-learn-intelex
-elif [[ $DATABRICKS_RUNTIME_VERSION =~ "9.0" ]] || [[ $DATABRICKS_RUNTIME_VERSION == "8.4" ]]; then
-    pip install intel-tensorflow==2.5.0
-    pip install scikit-learn-intelex
-elif [[ $DATABRICKS_RUNTIME_VERSION =~ $VERSION_9_P ]] || [[ $DATABRICKS_RUNTIME_VERSION == "10.0" ]]; then
+elif [[ $DATABRICKS_RUNTIME_VERSION == "9.1" ]] || [[ $DATABRICKS_RUNTIME_VERSION == "10.1" ]]; then
     pip install intel-tensorflow==2.6.0
+    pip install scikit-learn-intelex
+elif [[ $DATABRICKS_RUNTIME_VERSION == "10.2" ]] || [[ $DATABRICKS_RUNTIME_VERSION == "10.3" ]]; then
+    pip install intel-tensorflow==2.7.0
+    pip install scikit-learn-intelex
+elif [[ $DATABRICKS_RUNTIME_VERSION == "10.4" ]] || [[ $DATABRICKS_RUNTIME_VERSION =~ $VERSION_11_P ]]; then
+    pip install intel-tensorflow==2.8.0
     pip install scikit-learn-intelex
 fi
